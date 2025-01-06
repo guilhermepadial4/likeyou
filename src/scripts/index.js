502be1118972fad08bbe3b442f3b20e10731eae6
Guilherme Padial <guilherme.padial4@gmail.com>
2025-01-06 11:19:12 -0300

    update project

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,22 +2,18 @@ function toggleMenu() {
   const menu = document.getElementById("mobileMenu");
   const toggleButton = document.querySelector(".toggle-btn");
 
-  // Alterna a visibilidade do menu
   menu.classList.toggle("active");
 
-  // Alterna a classe para o ícone aberto/fechado
   toggleButton.classList.toggle("active");
 }
 
-// Fecha o menu se o usuário clicar fora do menu ou do botão
 document.addEventListener("click", function (event) {
   const menu = document.getElementById("mobileMenu");
   const toggleButton = document.querySelector(".toggle-btn");
 
-  // Verifica se o clique foi fora do menu e do botão de toggle
   if (!toggleButton.contains(event.target) && !menu.contains(event.target)) {
-    menu.classList.remove("active"); // Fecha o menu
-    toggleButton.classList.remove("active"); // Restaura o ícone
+    menu.classList.remove("active");
+    toggleButton.classList.remove("active");
   }
 });
 
@@ -25,7 +21,6 @@ function toggleSearchInput() {
   const searchIcon = document.querySelector(".search__icon");
   const searchInput = document.querySelector(".input__search");
 
-  // Adicionar ou remover a classe 'active' de forma condicional
   if (searchInput.classList.contains("active")) {
     searchIcon.classList.remove("active");
     searchInput.classList.remove("active");
@@ -36,3 +31,17 @@ function toggleSearchInput() {
 
   searchInput.focus();
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  window.handleLike = (button) => {
+    const likeCountSpan = button.querySelector(".like__count");
+    let count = parseInt(likeCountSpan.textContent, 10);
+    likeCountSpan.textContent = count + 1;
+  };
+
+  window.handleDislike = (button) => {
+    const dislikeCountSpan = button.querySelector(".dislike__count");
+    let count = parseInt(dislikeCountSpan.textContent, 10);
+    dislikeCountSpan.textContent = count + 1;
+  };
+});
